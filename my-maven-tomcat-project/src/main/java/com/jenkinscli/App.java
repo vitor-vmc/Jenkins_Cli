0efbd6b0798d694246656914dd6a70a4f561e451
vitor-vmc <vitormdc@hotmail.com>
2022-05-20 21:23:21 -0300

    build: first commit

---
+++ my-maven-tomcat-project/src/main/java/com/jenkinscli/App.java
@@ -0,0 +1,13 @@
+package com.jenkinscli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Meu Projeto Jenkins CLI - Vitor Campos 87229" );
+    }
+}
